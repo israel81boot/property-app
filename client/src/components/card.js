e5e4af15a950eced230abb97471a06346c1f4c19
israel Dembele <demisrael00@gmail.com>
2020-02-18 12:51:34 -0500

    stable

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -7,45 +7,47 @@ class Card extends Component {
     }
 
     componentDidMount() {
-        var hses = axios.get("/api/all").then(result =>{
-            return result;
+        var hses = axios.get("/api/all").then(result => {
+            console.log(result);
+            this.setState({ houses: result.data });
         });
-        console.log(hses);
-        this.setState({ houses : hses});
-      }
+    }
+
+    add = (house) => {
+        console.log("click: ", house);
+    }
 
     render() {
         return (<div>
-            <div class="card">
-                <div class="card-image">
-                    <div class="image is-4by3">
-                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
-                    </div>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <div class="image is-48x48">
-                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+            {this.state.houses.map((house, index) => {
+                return (
+                    <div key={index}>
+                        <div class="card">
+                            {/* <div class="card-image">
+                                <div class="image is-4by3 is-48x48">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </div>
+                            </div> */}
+                            <div class="card-content">
+                                <div class="media">
+                                    <div class="media-left">
+                                        <div class="image is-48x48">
+                                            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                                        </div>
+                                    </div>
+                                    <div class="media-content">
+                                        <p class="title is-4">{house.adress}</p>
+                                        <p class="subtitle is-6">Bed: {house.bed}</p>
+                                        <button onClick={() => this.add(house)}>Add</button>
+                                        
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="media-content">
-                            <p class="title is-4">John Smith</p>
-                            <p class="subtitle is-6">@johnsmith</p>
-                        </div>
                     </div>
+                )
+            })}
 
-                    <div class="content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Phasellus nec iaculis mauris.
-                    <a>@bulmaio</a>.
-                    <a href="#">#css</a>
-                        <a href="#">#responsive</a>
-                        <br />
-                        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                    </div>
-                </div>
-            </div>
         </div>)
     }
 }
